fb94c09b23813d24b87ae8c30fa333e2da3a3424
shreyavachhani26 <vachhanishreya30@gmail.com>
2025-09-07 20:26:51 +0530

    Add files via upload

---
+++ lab1/lab1.2.py
@@ -0,0 +1,7 @@
+
+# Print header
+print("a\t a^2\t a^3")
+
+# Loop through values of a
+for a in range(1, 5):
+    print(f"{a}\t {a**2}\t {a**3}")
---
+++ lab1/lab1.3.py
@@ -0,0 +1,9 @@
+
+numerator = (9.5 * 4.5) - (2.5 * 3)
+denominator = 45.5 - 3.5
+
+# Calculate result
+result = numerator / denominator
+
+# Display result
+print("The result of the expression is:", result)
